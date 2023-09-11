482d48ea9e7c9b452359b4c407e6449b4cbe7928
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-09-12 04:38:36 +0700

    Update tagall.py

--- FallenRobot/modules/tagall.py
+++ FallenRobot/modules/tagall.py
@@ -3,37 +3,44 @@ import asyncio
 from telethon import events
 from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
-from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
+from telethon.tl.types import ChannelParticipantAdmin
+from telethon.tl.types import ChannelParticipantCreator
 
 from FallenRobot import telethn as client
 
 spam_chats = []
 
 
-@client.on(events.NewMessage(pattern="^/tagall ?(.*)"))
-@client.on(events.NewMessage(pattern="^@all ?(.*)"))
+@client.on(events.NewMessage(pattern="^/tagall|@all|/all ?(.*)"))
 async def mentionall(event):
     chat_id = event.chat_id
     if event.is_private:
-        return await event.respond(
-            "__This command can be use in groups and channels!__"
-        )
+        return await event.respond("__Perintah ini hanya digunakan dalam grup dan channel.__*")
 
     is_admin = False
     try:
-        partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
+        partici_ = await client(GetParticipantRequest(
+            event.chat_id,
+            event.sender_id
+        ))
     except UserNotParticipantError:
         is_admin = False
     else:
-        if isinstance(
-            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
+        if (
+                isinstance(
+                    partici_.participant,
+                    (
+                            ChannelParticipantAdmin,
+                            ChannelParticipantCreator
+                    )
+                )
         ):
             is_admin = True
     if not is_admin:
-        return await event.respond("__Only admins can mention all!__")
+        return await event.reply("__Hanya admin yang dapat menjalankan perintah ini...__")
 
     if event.pattern_match.group(1) and event.is_reply:
-        return await event.respond("__Give me one argument!__")
+        return await event.reply("__Berikan beberapa teks atau balas pesan..__")
     elif event.pattern_match.group(1):
         mode = "text_on_cmd"
         msg = event.pattern_match.group(1)
@@ -42,30 +49,27 @@ async def mentionall(event):
         msg = await event.get_reply_message()
         if msg == None:
             return await event.respond(
-                "__I can't mention members for older messages! (messages which are sent before I'm added to group)__"
-            )
+                "Saya tidak bisa menyebut anggota untuk pesan lama! (pesan yang dikirim sebelum saya ditambahkan ke grup)")
     else:
-        return await event.respond(
-            "__Reply to a message or give me some text to mention others!__"
-        )
+        return await event.reply("__Berikan beberapa teks atau balas pesan..__")
 
     spam_chats.append(chat_id)
     usrnum = 0
-    usrtxt = ""
+    usrtxt = ''
     async for usr in client.iter_participants(chat_id):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
+        usrtxt += f"🫦 [{usr.first_name}](tg://user?id={usr.id})\n"
         if usrnum == 5:
             if mode == "text_on_cmd":
-                txt = f"{msg}\n{usrtxt}"
+                txt = f"{msg}\n\n{usrtxt}"
                 await client.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(usrtxt)
-            await asyncio.sleep(3)
+            await asyncio.sleep(2)
             usrnum = 0
-            usrtxt = ""
+            usrtxt = ''
     try:
         spam_chats.remove(chat_id)
     except:
@@ -74,32 +78,45 @@ async def mentionall(event):
 
 @client.on(events.NewMessage(pattern="^/cancel$"))
 async def cancel_spam(event):
-    if not event.chat_id in spam_chats:
-        return await event.respond("__There is no proccess on going...__")
     is_admin = False
     try:
-        partici_ = await client(GetParticipantRequest(event.chat_id, event.sender_id))
+        partici_ = await client(GetParticipantRequest(
+            event.chat_id,
+            event.sender_id
+        ))
     except UserNotParticipantError:
         is_admin = False
     else:
-        if isinstance(
-            partici_.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
+        if (
+                isinstance(
+                    partici_.participant,
+                    (
+                            ChannelParticipantAdmin,
+                            ChannelParticipantCreator
+                    )
+                )
         ):
             is_admin = True
     if not is_admin:
-        return await event.respond("__Only admins can execute this command!__")
-
+        return await event.reply("__Hanya admin yang dapat menjalankan perintah ini...__")
+    if not event.chat_id in spam_chats:
+        return await event.reply("__Tidak ada mention!__")
     else:
         try:
             spam_chats.remove(event.chat_id)
         except:
             pass
-        return await event.respond("__Stopped mention.__")
+        return await event.respond("__Dihentikan!__")
 
 
-__mod_name__ = "Tᴀɢ Aʟʟ"
+__mod_name__ = "ᴛᴀɢ-ᴀʟʟ"
 __help__ = """
-*Only for admins*
+──「 ᴛᴀɢ-ᴀʟʟ 」──
 
-❍ /tagall or @all '(reply to message or add another message) To mention all members in your group, without exception.'
+ZoidsRobot Can Be a Mention Bot for your group.
+
+Only admins can tag all.  here is a list of commands
+
+❂ /tagall or @all (membalas pesan atau menambahkan pesan lain) Untuk menyebutkan semua anggota di grup Anda, tanpa kecuali.
+❂ /cancel untuk membatalkan mention.
 """
